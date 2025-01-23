a90a172d6d7769a0908761e0f72a1caa4a598617
hassan ali <hassanali0111272@gmail.com>
2025-01-23 18:02:03 +0500

    file upload and retrieve

--- 02_practice/js files/files.js
+++ 02_practice/js files/files.js
@@ -1,6 +1,53 @@
 const fileInput = document.querySelector("#fileInput");
 const fileUploadBtn = document.querySelector("#fileUploadBtn");
 
-fileUploadBtn?.addEventListener("click",()=>{
-  console.log(fileInput)
+fileUploadBtn?.addEventListener("click", async ()=>{
+  const fileDetails = fileInput.files[0]
+  console.log(fileDetails)
+try {
+  const { data, error } = await supabase.storage
+    .from("usersprofile")
+    .upload(`public/${fileDetails.name}`, fileDetails, {
+      cacheControl: "3600",
+      upsert: false,
+    });
+  if (error) throw error;
+  if (data) {
+    console.log(data);
+  }
+} catch (error) {
+  console.log(error)
+}
+
 })
+
+const getAllFiles = async ()=>{
+  try {
+    const { data, error } = await supabase.storage
+      .from("usersprofile")
+      .list("public", {
+        limit: 100,
+        offset: 0,
+        sortBy: { column: "name", order: "asc" },
+      });
+      if(error) throw error;
+      if(data){
+        console.log(data)
+        try {
+          const { data : fileData , error : fileError } = supabase.storage
+            .from("usersprofile")
+            .getPublicUrl(`public/${data[1].name}`);
+          if(fileError) throw fileError;
+          if(fileData){
+            console.log(fileData)
+          }
+        } catch (error) {
+          
+        }
+      }
+
+  } catch (error) {
+    
+  }
+}
+window.onload = getAllFiles()
